0f0a76315cdd016035b121ddce0aa0b448a7b51e
Juan Francisco Cisneros <juanfrancistm2011@icloud.com>
2022-09-18 13:53:22 -0500

    emit bands to the users

--- models/band.js
+++ models/band.js
@@ -4,7 +4,7 @@ const {v4:uuidV4} = require('uuid');
 class Band{
 
     constructor(name = 'no-name') {
-        this.id = uuidV4; //Identificador unico
+        this.id = uuidV4(); //Identificador unico
         this.name = name;
         this.votes = 0;
     }
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,14 +1,26 @@
 
-const {io} = require('../index'); // Import socket.io
+const {io} = require('../index');
+const Bands = require("../models/bands");
+const Band = require("../models/band");
+
+const bands = new Bands(); // Create new Bands class
+console.log('init socket'); // Log that the socket has been initialized
+
+bands.addBand(new Band('Queen')); // Add a new band
+bands.addBand(new Band('Quevedo')); // Add a new band
+bands.addBand(new Band('Metalica')); // Add a new band
 
 //SOCKET MESSAGES
 io.on('connection', client => {
     console.log('Client connected...');
+
+    client.emit('active-bands', bands.getBands()); // Emit active bands
+
     client.on('disconnect', () => {
         console.log('User disconnected');
     }); // Listen for disconnect
 
-    client.on('mensaje',(payload)=>{
+/*    client.on('mensaje',(payload)=>{
         console.log('Mensaje recibido',payload);
         io.emit('mensaje',{admin:'LO RECIBI!!!'});
     })
@@ -17,6 +29,6 @@ io.on('connection', client => {
         //console.log('New message', payload);
         //io.emit('newMessage', payload); // Emit newMessage to all clients
         client.broadcast.emit('newMessage', payload); // Emit newMessage to all clients except the one that sent it
-    }); // Listen for newMessage
+    }); // Listen for newMessage*/
 
 }); // Listen for connection
